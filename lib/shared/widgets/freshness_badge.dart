92774e3c7ef77674d9881b13d3ca14df6178e0f8
Mahyar <mahyarardameh10@gmail.com>
2026-07-31 17:53:01 +0300

    Minor changes to user interface.

--- lib/shared/widgets/freshness_badge.dart
+++ lib/shared/widgets/freshness_badge.dart
@@ -26,7 +26,7 @@ class FreshnessBadge extends StatelessWidget {
         vertical: SangakDimens.spacing4,
       ),
       decoration: BoxDecoration(
-        color: token.color.withValues(alpha: 0.35),
+        color: token.color.withValues(alpha: 0.45),
         borderRadius: BorderRadius.circular(SangakDimens.radiusPill),
       ),
       child: Row(
